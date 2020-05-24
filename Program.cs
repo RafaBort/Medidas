e8c0e653efb3bcda1041e116d82721eba07365f9
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-05-24 19:21:37 -0300

    exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma medida em metro...");
+            string metros;
+            metros = Console.ReadLine();
+
+            Double medidas1 = Convert.ToDouble(metros);
+            Double CM = 100;
+            Double KM = 1000;
+
+            Double multiplicacao = medidas1 * CM;
+            Double igual = medidas1;
+            Double divisao = medidas1 / KM;
+
+            Console.WriteLine("em cm" + " " + multiplicacao);
+            Console.WriteLine("em metros" + " " + igual);
+            Console.WriteLine("em km" + " " + divisao);
+
+        }
+    }
+}
